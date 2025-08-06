1fb342c5518069628c9659601218681e77131ba3
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-08-06 14:00:26 +0530

    Datatypes completed

---
+++ Day-02/Datatypes.js
@@ -0,0 +1,19 @@
+//primtive
+a = 10; //int
+b = "apple"; //string
+c = "";
+d = null;
+
+//Non primitive
+e = [1, 2, 3, 4];
+f = { name: "deepak", age: 27 };
+
+console.log("Primitve");
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log("Non Primitive");
+console.log(typeof e);
+console.log(typeof f);
