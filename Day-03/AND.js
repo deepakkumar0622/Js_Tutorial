379733b71b20c9454cbe39135f87cf167a75d517
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-08-07 11:54:34 +0530

    Logical Operators

---
+++ Day-03/AND.js
@@ -0,0 +1,8 @@
+var a = 20;
+var b = 20;
+
+if (a && b == 20) {
+  console.log("Same");
+} else {
+  console.log("Not Same");
+}
---
+++ Day-03/OR.js
@@ -0,0 +1,8 @@
+let a = true;
+let b = false;
+
+if (a || b) {
+  console.log("False");
+} else {
+  console.log("true");
+}
